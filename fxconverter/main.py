16e7d499b1e822f3b339b055a991eba3f41ec0f2
Anatoly Bugakov <anatoly.bugakov@outlook.com>
2024-12-11 19:24:50 +0000

    returned schema

--- fxconverter/main.py
+++ fxconverter/main.py
@@ -1,13 +1,22 @@
 from enum import Enum
 import uvicorn
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+# Query Parameters
+# CoinBase only supports three currencies
+# so in this case best to restrict
 class CCY(Enum):
     USD = "usd"
     EUR = "eur"
     GBP = "gbp"
+
+# Returned value
+class Converted(BaseModel):
+    quantity: float
+    ccy: CCY
 
 @app.get("/convert/")
 async def convert(ccy_from: CCY, ccy_to: CCY, quantity: float = 1.0):
